b03106d25191ec9a56fc96af22e21e9303e53e31
delaquash <olaide1191@gmail.com>
2026-01-17 21:55:44 +0100

    Refactor config.go to improve structure and readability by organizing imports and defining the Config type

--- carezo_backend/configs/config.go
+++ carezo_backend/configs/config.go
@@ -1,1 +1,21 @@
 package configs
+
+
+import (
+	"logs"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost 		string
+	DBPort 		string
+	DBUser 		string
+	DBPassword 	string
+	DBName		string
+
+	// Redis Setting
+}
