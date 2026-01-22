596a8544b5f3d6e6fa7e3df22dbdd84eec32ee0d
delaquash <olaide1191@gmail.com>
2026-01-22 12:21:13 +0100

    Implement complete profile, login, forgot password, and reset password functionalities

--- carezo_backend/internal/handlers/auth_handler.go
+++ carezo_backend/internal/handlers/auth_handler.go
@@ -79,3 +79,120 @@ func (h *AuthHandler) VerifyOTP(c *gin.Context) {
 		nil,
 	)
 }
+
+// POST /api/auth/complete-profile
+func (h *AuthHandler) CompleteProfile(c *gin.Context) {
+	// Get phone nu,ber from query parameter (this is from otp verification)
+	phoneNumber := c.Query("phone")
+
+	if phoneNumber == "" {
+		response.Error(
+			c,
+			http.StatusBadRequest,
+			"Phone Number is required",
+		)
+		return
+	}
+
+	var req model.CompleteProfileRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.Error(
+			c,
+			http.StatusBadRequest,
+			"Invalid request data: " +err.Error(),
+		)
+		return
+	}
+	err := h.authService.CompleteProfile(phoneNumber, &req)
+	if err != nil {
+		response.Error(
+			c,
+			http.StatusBadRequest,
+			err.Error(),
+		)
+		return
+	}
+	response.Success(
+		c,
+		http.StatusOK,
+		"Profile Completed Successfuly. You can login now",
+		nil,
+	)
+}
+
+
+// POST /api/auth/login
+func (h *AuthHandler) Login(c *gin.Context) {
+	var req model.LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.Error(
+			c,
+			http.StatusBadRequest,
+			"Invalid request data: "+err.Error(),
+		)
+		return
+	}
+	authResponse, err := h.authService.Login(&req)
+	if err != nil {
+		response.Error(
+			c,
+			http.StatusUnauthorized,
+			err.Error(),
+		)
+		return
+	}
+
+	response.Success(
+		c,
+		http.StatusOK,
+		"Login Successful",
+		authResponse,
+	)
+}
+
+// POST /api/auth/forgot-password
+func (h *AuthHandler) ForgotPassword(c *gin.Context){
+	var req model.ForgotPasswordRequest
+	if err := c.ShouldBindJSON(&req); err!= nil {
+		response.Error(
+			c,
+			http.StatusBadRequest,
+			"Invalid request data: "+err.Error(),
+		)
+		return
+	}
+	err := h.authService.ForgotPassword(&req)
+	if err != nil {
+		// error is logged
+		c.Error(err)
+	}
+
+
+	response.Success(c, http.StatusOK, "If your email exists, you will receive a password reset link.", nil)
+}
+
+
+// POST /api/auth/reset-password
+func (h *AuthHandler) ResetPassword(c *gin.Context) {
+	var req model.ResetPasswordRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.Error(
+			c,
+			http.StatusBadRequest,
+			"Invalid Request data: "+err.Error())
+			return
+	}
+	err := h.authService.ResetPassword(&req)
+	if err != nil {
+		response.Error(
+			c,
+			http.StatusBadRequest, err.Error())
+		return
+	}
+	response.Success(
+		c,
+		http.StatusOK,
+		"Password reset successful. You can login with your new password", nil,
+	)
+
+}
