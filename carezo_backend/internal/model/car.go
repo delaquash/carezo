eda1b3b4bf2cb466b09f78386388f3c990a00319
delaquash <olaide1191@gmail.com>
2026-01-26 21:35:56 +0100

    feat: still adding user model

---
+++ carezo_backend/internal/model/car.go
@@ -0,0 +1,20 @@
+package models
+
+type Car struct {
+	ID string `json:"id" db:"id"`
+
+	Model 			string  `json:"model" db:"model"`
+	Brand			string  `json:"brand" db:"brand"`
+	Year			int		`json:"year"  db:"year"`
+	Color   		string	`json:"color" db:"color"`
+	LicensePlace	string	`json:"licence_place" db:"license_plate"`
+
+	// specification
+	EngineOutput     *string `json:"engine_output,omitempty" db:"engine_output"` 
+	Transmission	string	 `json:"transmission" db:"transmission"`
+	FuelType		string   `json:"fuel_type" db:"fuel_type"`
+	SeatingCapacity int      `json:"seating_capacity" db:"seating_capacity"`
+	MaximumSpeed   *int      `json:"maximum_speed,omitempty" db:"maximum_speed"`
+	Mileage			int		 `json:`
+
+}
