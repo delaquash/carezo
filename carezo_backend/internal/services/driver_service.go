99e6acb0dc0236cc1b019488f10dcf136c463828
delaquash <olaide1191@gmail.com>
2026-01-31 11:32:33 +0100

    feat: implement update driver logic in driver service component

--- carezo_backend/internal/services/driver_service.go
+++ carezo_backend/internal/services/driver_service.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/delaquash/carezo/internal/database"
@@ -95,3 +96,129 @@ func(s *DriverService) GetDriverByID(driverID string) (*models.Driver, error) {
 	return &driver, nil
 	
 }
+
+
+
+func (s *DriverService) UpdateDriver(driverID string, req *models.UpdateDriverRequest) (*models.Driver, error) {
+	// check if driver exist
+	_, err := s.GetDriverByID(driverID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// dynamic update query that only update provided fields
+	var updates []string
+	var args []interface{}
+
+	argCount := 1
+
+	if req.FirstName != nil {
+		updates = append(updates, fmt.Sprintf("first_name = $%d", argCount))
+		args = append(args, *req.FirstName)
+		argCount++
+	}
+	if req.LastName != nil {
+		updates = append(updates, fmt.Sprintf("last_name = $%d", argCount))
+		args = append(args, *req.LastName)
+		argCount++
+	}
+	if req.Age != nil {
+		updates = append(updates, fmt.Sprintf("age = $%d", argCount))
+		args = append(args, *req.Age)
+		argCount++
+	}
+	if req.Gender != nil {
+		updates = append(updates, fmt.Sprintf("gender = $%d", argCount))
+		args = append(args, *req.Gender)
+		argCount++
+	}
+	if req.State != nil {
+		updates = append(updates, fmt.Sprintf("state = $%d", argCount))
+		args = append(args, *req.State)
+		argCount++
+	}
+	if req.Religion != nil {
+		updates = append(updates, fmt.Sprintf("religion = $%d", argCount))
+		args = append(args, *req.Religion)
+		argCount++
+	}
+	if req.Complexion != nil {
+		updates = append(updates, fmt.Sprintf("complexion = $%d", argCount))
+		args = append(args, *req.Complexion)
+		argCount++
+	}
+	if req.Height != nil {
+		updates = append(updates, fmt.Sprintf("height = $%d", argCount))
+		args = append(args, *req.Height)
+		argCount++
+	}
+	if req.PhoneNumber != nil {
+		updates = append(updates, fmt.Sprintf("phone_number = $%d", argCount))
+		args = append(args, *req.PhoneNumber)
+		argCount++
+	}
+	if req.Email != nil {
+		updates = append(updates, fmt.Sprintf("email = $%d", argCount))
+		args = append(args, *req.Email)
+		argCount++
+	}
+	if req.LicenseNumber != nil {
+		updates = append(updates, fmt.Sprintf("license_number = $%d", argCount))
+		args = append(args, *req.LicenseNumber)
+		argCount++
+	}
+	if req.LicenseExpiryDate != nil {
+		expiryDate, err := time.Parse("1991-01-11", *req.LicenseExpiryDate)
+
+		if err != nil {
+			return nil, errors.New("Invalid license expiry date format")
+		}
+		updates = append(updates, fmt.Sprintf("Licence expiry date = $%d", argCount))
+		args = append(args, expiryDate)
+		argCount++
+	}
+	if req.IsAvailable != nil {
+		updates = append(updates, fmt.Sprintf("is_available = $%d", argCount))
+		args = append(args, *req.IsAvailable)
+		argCount++
+	}
+	if req.Status != nil {
+		updates = append(updates, fmt.Sprintf("status = $%d", argCount))
+		args = append(args, *req.Status)
+		argCount++
+	}
+	if req.Languages != nil {
+		languagesJSON, _ := json.Marshal(req.Languages)
+		updates = append(updates, fmt.Sprintf("Languages = $%d", argCount))
+		args = append(args, languagesJSON)
+		argCount++
+	}
+	if len(updates) == 0 {
+		return nil, errors.New("No fields to update")
+	}
+	
+	// add updated_at
+	updates = append(updates, "updated_at = CURRENT_TIMESTAMP")
+
+	// add driverID to args
+	args = append(args, driverID)
+
+	// execute update fields
+	query := fmt.Sprintf(
+		`
+		UPDATE drivers
+		SET %s
+		WHERE id = $%d AND deleted_at IS NULL
+		RETURNING *
+		`, strings.Join(updates, ", "),argCount)
+
+	var driver models.Driver
+	err = database.DB.Get(&driver, query, args...)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to update driver: %w", err)
+	}
+	return &driver, nil
+}
+
