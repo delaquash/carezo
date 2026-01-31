4f0ff90bfd59a4b825a9398e451c215d17845ea8
delaquash <olaide1191@gmail.com>
2026-01-31 15:30:14 +0100

    feat: implement search for driver and pagination in car service

--- carezo_backend/internal/services/driver_service.go
+++ carezo_backend/internal/services/driver_service.go
@@ -22,15 +22,15 @@ func NewDriverService() *DriverService {
 
 
 // Admin to create driver
-func (s *DriverService)CreateDriver(req *models.CreateDriverRequest) (*models.Driver, error) {
-	// check if vehicle exist using license number
+func (s *DriverService) CreateDriver(req *models.CreateDriverRequest) (*models.Driver, error) {
+	// check if vehicle exist using license number and has not been soft-deleted (deleted_at IS NULL)
 	var exists bool
 	query := `SELECT EXISTS(SELECT 1 FROM drivers WHERE license_number = $1 AND deleted_at is NULL)`
 
 	err := database.DB.Get(&exists, query, req.LicenseNumber)
 
 	if err != nil {
-		return nil, fmt.Errorf("Data error: &w", err)
+		return nil, fmt.Errorf("Data error: %w", err)
 	}
 
 	if exists {
@@ -220,5 +220,174 @@ func (s *DriverService) UpdateDriver(driverID string, req *models.UpdateDriverRe
 		return nil, fmt.Errorf("Failed to update driver: %w", err)
 	}
 	return &driver, nil
+}
+
+
+// Soft delete driver
+func(s *DriverService) DeleteDriver(driverID string) error {
+
+	// check if driver exist
+	_, err := s.GetDriverByID(driverID)
+
+	if err != nil {
+		return err
+	}
+
+	// soft delete if driver exist
+	query := `UPDATE drivers SET deleted_at = CURRENT_TIMESTAMP WHERE id = $1 AND deleted_at IS NULL`
+
+	result, err := database.DB.Exec(query, driverID)
+
+	if err != nil {
+		return fmt.Errorf("Failed to delete driver: %w", err)
+	}
+
+	rows, _ := result.RowsAffected()
+
+	if rows == 0 {
+		return errors.New("Driver not found")
+	}
+	return nil
+}
+
+
+// Search for drivers and filter with pagination
+func( s*CarService)SearchDrivers(req *models.SearchDriversRequest)(*models.DriverListResponse, error){
+	var conditions []string
+	var args []interface{}
+	argCount := 1
+
+	// exclude deleted acrs
+	conditions = append(conditions, "deleted_at IS NULL")
+
+	if req.Gender != nil {
+		conditions = append(conditions, fmt.Sprintf("gender = $%d", argCount))
+		args = append(args, *req.Gender)
+		argCount++
+	}
+	if req.State != nil {
+		conditions = append(conditions, fmt.Sprintf("LOWER(state) = LOWER($%d)", argCount))
+		args = append(args, *req.State)
+		argCount++
+	}
+	if req.Religion != nil {
+		conditions = append(conditions, fmt.Sprintf("LOWER(religion) = LOWER($%d)", argCount))
+		args = append(args, *req.Religion)
+		argCount++
+	}
+	if req.Complexion != nil {
+		conditions = append(conditions, fmt.Sprintf("LOWER(complexion) = LOWER($%d)", argCount))
+		args = append(args, *req.Complexion)
+		argCount++
+	}
+	if req.MinAge != nil {
+		conditions = append(conditions, fmt.Sprintf("age >= $%d", argCount))
+		args = append(args, *req.MinAge)
+		argCount++
+	}
+	if req.MaxAge != nil {
+		conditions = append(conditions, fmt.Sprintf("age <= $%d", argCount))
+		args = append(args, *req.MaxAge)
+		argCount++
+	}
+	if req.MinHeight != nil {
+		conditions = append(conditions, fmt.Sprintf("height >= $%d", argCount))
+		args = append(args, *req.MinHeight)
+		argCount++
+	}
+	if req.MaxHeight != nil {
+		conditions = append(conditions, fmt.Sprintf("height <= $%d", argCount))
+		args = append(args, *req.MaxHeight)
+		argCount++
+	}
+	if req.MinExperience != nil {
+		conditions = append(conditions, fmt.Sprintf("years_of_experience >= $%d", argCount))
+		args = append(args, *req.MinExperience)
+		argCount++
+	}
+	if req.MinRating != nil {
+		conditions = append(conditions, fmt.Sprintf("average_rating >= $%d", argCount))
+		args = append(args, *req.MinRating)
+		argCount++
+	}
+	if req.IsAvailable != nil {
+		conditions = append(conditions, fmt.Sprintf("is_available = $%d", argCount))
+		args = append(args, *req.IsAvailable)
+		argCount++
+	}
+	if req.Language != nil {
+		conditions = append(conditions, fmt.Sprintf("languages::text ILIKE $%d", argCount))
+		args = append(args, "%"+*req.Language+"%")
+		argCount++
+	}
+// count total
+whereClause := strings.Join(conditions, " AND ")
+countQuery := fmt.Sprintf("SELECT COUNT(*) FROM drivers HWERE %s", whereClause)
+
+var total int
+err := database.DB.Get(&total, countQuery, args...)
+
+if err != nil {
+	return nil, fmt.Errorf("Failed to count drivers: %w", err)
+}
+
+// Build order by
+
+orderBy := "created_at DESC"
+
+if req.SortBy != "" {
+	order := "ASC"
+	if req.OrderBy == "desc" {
+		order = "DESC"
+	}
+	orderBy = fmt.Sprintf("%s %s", req.SortBy, order)
+}
+
+// 4. Pagination
+	page := req.Page
+	if page < 1 {
+		page = 1
+	}
+	perPage := req.PerPage
+	if perPage < 1 {
+		perPage = 20
+	}
+	offset := (page - 1) * perPage
+
+	// query drivers
+	query := fmt.Sprintf(`
+		SELECT * FROM drivers
+		WHERE %s
+		ORDER BY %s
+		LIMIT $%d OFFSET $%d
+	`, whereClause, orderBy, argCount, argCount+1)
+	args = append(args, perPage, offset)
+
+	var drivers []*models.Driver
+
+	err = database.DB.Select(&drivers, query, args...)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch drivers: %w", err)
+	}
+	// calculate total pages
+	totalPages := (total + perPage -1) / perPage
+
+	// response
+	return &models.DriverListResponse{
+		Drivers: drivers,
+		Pagination: models.PaginationMeta{
+			Page: page,
+			PerPage: perPage,
+			Total: total,
+			TotalPages: totalPages,
+		},
+		Filters: map[string]interface{}{
+			"gender": req.Gender,
+			"state": req.State,
+			"religion": req.Religion,
+			"complexion": req.Complexion,
+		},
+	}, nil
 }
 
