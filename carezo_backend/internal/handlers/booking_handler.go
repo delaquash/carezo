4100ed6bd4ab8ce8d74b50664b196e47c20245ea
delaquash <olaide1191@gmail.com>
2026-02-05 11:27:52 +0100

    feat(booking): implement BookingHandler structure and CreateBooking method skeleton

--- carezo_backend/internal/handlers/booking_handler.go
+++ carezo_backend/internal/handlers/booking_handler.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	"net/http"
+
+	models "github.com/delaquash/carezo/internal/model"
+	"github.com/delaquash/carezo/internal/services"
+	response "github.com/delaquash/carezo/pkg"
+	"github.com/gin-gonic/gin"
+)
+
+
+type BookingHandler struct {
+	bookingService *services.BookingService 
+}
+
+func NewBookingHandler() *BookingHandler {
+	return &BookingHandler{
+		bookingService: services.NewBookingService(),
+	}
+}
+
+
+func (h *BookingHandler) CreateBooking(c *gin.Context) {
+	// get auth user from jwt middleware
+	userID, exists := c.Get("user_id")
+
+	if !exists {
+		response.Error(c, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
+	var req models.CreateBookingRequest
+
+	
+	
+}
