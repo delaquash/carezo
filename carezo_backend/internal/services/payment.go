46316931c994070569f59548ddfc2ab820e9ceef
delaquash <olaide1191@gmail.com>
2026-02-11 23:52:54 +0100

    feat: add payment integration model with paystack

--- carezo_backend/internal/services/payment.go
+++ carezo_backend/internal/services/payment.go
@@ -1,5 +1,19 @@
 package services
 
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/delaquash/carezo/internal/database"
+	models "github.com/delaquash/carezo/internal/model"
+)
+
 type PaymentService struct {
 	paystackSecretKey string
 	paystackBaseURL string
@@ -45,7 +59,97 @@ type PaystackVerifyResponse struct {
 }
 
 
-func (s *PaymentService) InitializePayment(bookingID string, userEmail string){
-	
+func (s *PaymentService) InitializePayment(bookingID string, userEmail string)(*models.PaymentInitializeResponse, error){
+	var booking models.Booking
+
+	query := `SELECT * FROM bookings WHERE id =$1`
+
+	err := database.DB.Get(&booking, query, bookingID)
+
+	if err != nil {
+		if err == sql.ErrNoRows{
+			return nil, errors.New("Booking not found")
+		}
+		return nil, fmt.Errorf("Database error: %w", err)
+	}
+
+	// check if payment already exist
+	if booking.PaymentStatus == models.PaymentStatusCompleted {
+		return nil, errors.New("Payment already completed for this booking")
+	}
+
+
+	// convert amount from kobo to naira
+	// because paystack charges in kobo
+	amountInKobo := int(booking.TotalAmount * 100)
+
+	// request payload
+	payload := map[string]interface{} {
+		"email": userEmail,
+		"amount": amountInKobo,
+		"metadata": map[string]string {
+			"booking_id": bookingID,
+		},
+	}
+
+	payloadBytes, err := json.Marshal(payload)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to marshal payment payload: %w", err)
+	}
+
+	// calling paystack api
+	url := s.paystackBaseURL + "/transaction/initialize"
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create request: %w", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+s.paystackSecretKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 30 * time.Second}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to initialize payment: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read response: %w", err)
+	}
+
+	var paystackRep PaystackInitializeResponse
+
+	if err := json.Unmarshal(body, &paystackRep);  err != nil {
+		return nil, fmt.Errorf("Failed to parse Paystack response: %w", err)
+	}
+
+	if !paystackRep.Status {
+		return nil, fmt.Errorf("Paystack error: %s", paystackRep.Message)
+	}
+
+
+	// store payment reference in the booking so that it can be used during verification
+	err = s.bookingService.StorePaymentReference(bookingID, paystackRep.Data.Reference)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to store payment reference: %w", err)
+	}
+
+
+	// return payment url and reference
+
+	return &models.PaymentInitializeResponse{	
+		AuthorizationURL: paystackRep.Data.AuthorizationURL,
+		AccessCode: paystackRep.Data.AccessCode,
+		Reference: paystackRep.Data.Reference,
+	}, nil
 }
 	
