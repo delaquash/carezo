5474c8086d65d9febf80b7c9a52d8dff423e4f6b
delaquash <olaide1191@gmail.com>
2026-02-24 11:37:37 +0100

    feat: initialize carezo_mobile project with essential files and reset script
    - Added package.json with dependencies and scripts for the project.
    - Created reset-project.js script to reset the project structure and manage existing files.
    - Included tsconfig.json for TypeScript configuration.

---
+++ carezo_backend/README.md
@@ -0,0 +1,9 @@
+Payment Bank: Flutterwave Comet (100137272)
+
+Payment Remark: Online Payment via Flutterwave: Txn Ref: (100004260218212827152709277215)
+
+Payment Mode: ONLINE PAYMENT
+
+receipt Staff: flutterwave_api
+Payment Date: 18/02/2026
+Auto-Payment date: 18/02/2026
---
+++ carezo_mobile/assets/images/index.ts
@@ -0,0 +1,13 @@
+const assets = {
+    eye : require("./eye.png"),
+    logo: require("./logo.png"),
+    ferrari: require("./ferrari.png"),
+    carbg: require("./OnboardingScreenbg1.png"),
+    whiteCar: require("./whitecar.png"),
+    overlayBg: require("./overlay-bg.png"),
+    whiteFerrari: require("./white_ferrari.png"),
+    logoBlack: require("./logo_black.png"),
+
+}
+
+export default assets;
